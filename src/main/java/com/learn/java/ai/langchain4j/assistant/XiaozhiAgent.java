13cf2f4530972dcbf38e047e8cef3a99b8d3df76
JP3000 <2691967523@qq.com>
2026-03-12 11:05:57 +0800

    update streaming output

--- src/main/java/com/learn/java/ai/langchain4j/assistant/XiaozhiAgent.java
+++ src/main/java/com/learn/java/ai/langchain4j/assistant/XiaozhiAgent.java
@@ -4,11 +4,15 @@ import dev.langchain4j.service.MemoryId;
 import dev.langchain4j.service.SystemMessage;
 import dev.langchain4j.service.UserMessage;
 import dev.langchain4j.service.spring.AiService;
+import reactor.core.publisher.Flux;
+
 import static dev.langchain4j.service.spring.AiServiceWiringMode.EXPLICIT;
 
 @AiService(
         wiringMode = EXPLICIT,
-        chatModel = "qwenChatModel",
+//        chatModel = "qwenChatModel",
+        // streaming chat model
+        streamingChatModel = "qwenStreamingChatModel",
         chatMemoryProvider = "chatMemoryProviderXiaozhi",
         tools = "appointmentTools",
 //        contentRetriever = "contentRetrieverXiaozhi"
@@ -17,5 +21,6 @@ import static dev.langchain4j.service.spring.AiServiceWiringMode.EXPLICIT;
 public interface XiaozhiAgent {
 
     @SystemMessage(fromResource = "zhaozhi-prompt-template.txt")
-    String chat(@MemoryId Long memoryId, @UserMessage String userMessage);
+//    String chat(@MemoryId Long memoryId, @UserMessage String userMessage);
+    Flux<String> chat(@MemoryId Long memoryId, @UserMessage String userMessage);
 }
